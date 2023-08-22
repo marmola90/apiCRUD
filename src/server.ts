aafed97b26012a7a7fb94d3a801a0624757a2d83
marmola90 <amarmol20@gmail.com>
2023-08-22 15:18:05 -0600

    adding statics react files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import path from 'path'
 
 // import v1RouterXnet from './v1/routes/xnetRoutes'
 import { router } from './v1/routes'
@@ -9,8 +10,15 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const indexPath = path.resolve(__dirname, 'dist', 'index.html')
+
+app.use(express.static(path.resolve(__dirname, 'dist')))
 // app.use('/api/v1/', v1RouterXnet)
 app.use('/api/v1/', router)
+
+app.get('*', (_req, res) => {
+  res.sendFile(indexPath)
+})
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server running on port ${process.env.APP_PORT as string}`)
